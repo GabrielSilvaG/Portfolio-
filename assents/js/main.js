40c9db12f83509558632b471aff7d4ec372db2a7
Gabriel Silva <gsg.gabrielsilva96@gmail.com>
2024-03-26 17:00:26 -0300

    atualizando para baixar novamente

--- assents/js/main.js
+++ assents/js/main.js
@@ -1,6 +1,29 @@
-(async () => {
+function atualizarPerfil(perfilData) {
+    const foto = document.getElementById('perfil.foto')
+    foto.src = perfilData.foto
+    foto.alt = perfilData.name
 
-    const perfilData = await buscarDadosPerfil()
-    console.log(perfilData)
 
-})()
+    const nome = document.getElementById('perfil.nome')
+    nome.innerText = perfilData.nome
+
+    const trabalho = document.getElementById('perfil.trabalho')
+    trabalho.innerText = perfilData.trabalho
+
+    const localizacao = document.getElementById('perfil.localizacao')
+    localizacao.innerText = perfilData.localizacao
+
+    const contato = document.getElementById('perfil.contato')
+    contato.innerText = perfilData.contato
+    contato.href = `tel:${perfilData.contato}`
+
+    const email = document.getElementById('perfil.email')
+    email.innerText = perfilData.email
+    email.href = `mailto:${perfilData.email}`
+}
+
+    (async () => {
+
+        const perfilData = await buscarDadosPerfil()
+        atualizarPerfil(perfilData)
+    })()
