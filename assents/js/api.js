ec3e87f7e63e33f74b045d4005fe49563d5d5588
Gabriel Silva <gsg.gabrielsilva96@gmail.com>
2024-03-26 17:48:01 -0300

    teste 2

--- assents/js/api.js
+++ assents/js/api.js
@@ -1,6 +1,6 @@
 async function buscarDadosPerfil() { /* async function buscarDadosPerfil = Define uma função assíncrona chamada */
 
-    const url = 'https://raw.githubusercontent.com/GabrielSilvaG/Portfolio-/master/data/profile.json?token=GHSAT0AAAAAACOYBXIJZD4OWNKTNA45EPVQZQDFJQQ' /*const url = Declara uma constante chamada url que contém o endereço da API de onde os dados serão buscados.*/
+    const url =`https://raw.githubusercontent.com/GabrielSilvaG/Portfolio-/master/data/profile.json?token=GHSAT0AAAAAACOYBXIJKYNLHEE7P64UMJ6EZQDGFDA`; /*const url = Declara uma constante chamada url que contém o endereço da API de onde os dados serão buscados.*/
     const fetching = await fetch(url) /*const fetching = await fetch(url) = Usa a função fetch para fazer uma requisição HTTP para a URL especificada. O await faz com que o JavaScript espere até que a requisição seja concluída antes de prosseguir.*/
     return await fetching.json() /*return await fetching.json() = Converte a resposta da requisição em JSON utilizando o método .json(). O await aqui também é usado para esperar que essa conversão seja concluída antes de retornar o resultado da função.*/
 }
